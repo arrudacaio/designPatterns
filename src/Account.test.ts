811cfa7fd64669b109ab433c36aa1a18241d20aa
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2022-01-01 17:10:37 -0300

    test: adds test for create an account and make a deposit

--- src/Account.test.ts
+++ src/Account.test.ts
@@ -11,3 +11,16 @@ test('Should create an account', () => {
 
   expect(account.getBalance()).toBe(0)
 })
+
+
+test('Should create an account and make a deposit', () => {
+  const account = new AccountBuilder('132.423.904-02')
+    .setBank('033')
+    .setBranch('0001')
+    .setAccount('123456-1')
+    .build()
+
+
+  account.credit(1000)
+  expect(account.getBalance()).toBe(1000)
+})
