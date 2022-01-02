2213495c12b01b8e30a8a8cbe156f1bf2d8eb1c0
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2022-01-01 23:58:29 -0300

    test: adds test to create two accounts and make transfer

--- src/Account.test.ts
+++ src/Account.test.ts
@@ -1,5 +1,6 @@
 import Account from "./Account"
 import AccountBuilder from "./AccountBuilder"
+import TransferService from "./TransferService"
 
 test('Should create an account', () => {
   const account = new AccountBuilder('132.423.904-02')
@@ -36,4 +37,27 @@ test('Should create an account and make a debit', () => {
   account.credit(1000)
   account.debit(500)
   expect(account.getBalance()).toBe(500)
+})
+
+
+test('Should create two accounts and make a transfer', () => {
+  const accountFrom = new AccountBuilder('132.423.904-02')
+    .setBank('033')
+    .setBranch('0001')
+    .setAccount('123456-1')
+    .build()
+
+  const accountTo = new AccountBuilder('764.233.111-53')
+    .setBank('033')
+    .setBranch('0042')
+    .setAccount('124562-1')
+    .build()
+
+
+  accountFrom.credit(1000)
+  accountTo.credit(500)
+  const transferService = new TransferService()
+  transferService.transfer(accountFrom, accountTo, 700)
+  expect(accountFrom.getBalance()).toBe(300)
+  expect(accountTo.getBalance()).toBe(1200)
 })
